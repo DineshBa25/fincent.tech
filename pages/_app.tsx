5254785cc559db71eecae0a300682777c5830911
dineshba25 <dineshba63@gmail.com>
2023-01-30 00:09:32 -0600

    added auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
+import 'rsuite/dist/rsuite.min.css';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CustomProvider } from 'rsuite';
+import {AuthProvider} from "./FirebaseAuthContext";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return(
+    <AuthProvider>
+      <CustomProvider theme="dark">
+        <Component {...pageProps} />
+      </CustomProvider>
+    </AuthProvider>
+  );
 }
